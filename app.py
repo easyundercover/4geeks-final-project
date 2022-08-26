7e4c035666966d88e19deff77595b3506b039837
easyundercover <belenfpineyro@gmail.com>
2022-08-26 22:29:00 +0000

    Update

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import plotly.express as px 
 import streamlit as st
+import streamlit as st
+from wordcloud import WordCloud
 
 @st.cache
 def load_data():
@@ -13,19 +15,37 @@ def load_data():
                             'sentimiento',
                             'ReplyCount',
                             'RetweetCount',
-                            'LikeCount']]
+                            'LikeCount',
+                            'tweets_clean']]
     df = df_interim.copy()
     return df
 
 df_ch = load_data()
 
+neg = df_ch[df_ch['sentimiento'] == 'NEG']
+pos = df_ch[df_ch['sentimiento'] == 'POS']
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title('Tweets de prensa escrita uruguaya')
 st.subheader('Dataframe')
 st.dataframe(df_ch)
-st.subheader('Nube de palabras')
-col1 = st.columns(1)
-#fig1 = px.histogram(df_ch, x='LikeCount')
-#col1.plotly_chart(fig1, use_container_width=True)
+st.subheader('Palabras asociadas al sentimiento negativo')
+text = ' '.join(i for i in neg.tweets_clean)
+wordcloud = WordCloud(max_words=100, background_color='White').generate(text)
+plt.style.use('classic')
+plt.figure(figsize = (12,12))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.show()
+st.pyplot()
+
+st.subheader('Palabras asociadas al sentimiento positivo')
+text = ' '.join(i for i in pos.tweets_clean)
+wordcloud = WordCloud(max_words=100, background_color='White').generate(text)
+plt.style.use('classic')
+plt.figure(figsize = (12,12))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.show()
+st.pyplot()
 
 # Heroku uses the last version of python, but it conflicts with 
 # some dependencies. Low your version by adding a runtime.txt file
